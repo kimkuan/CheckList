b213ecc2a7bfca7226227d289fee59e960514636
sunhye <wkadnsj@gmail.com>
2021-09-27 21:09:53 +0900

    [FE] feat: :sparkles: 필터 탭 추가

--- frontend/src/views/Search.vue
+++ frontend/src/views/Search.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="search">
+    <div class="searchFilter">
+      <searchFilter />
+    </div>
     <div class="productCard">
       <ProductCard v-for="product in products" :product="product" :key="product.id" />
     </div>
@@ -11,12 +14,14 @@
 </style>
 
 <script>
+import SearchFilter from "../components/search/SearchFilter.vue";
 import ProductCard from "../components/product/ProductCard.vue";
 import {computed} from 'vue';
 import {useStore} from "vuex";
 export default {
   name: 'Search',
   components: {
+    SearchFilter,
     ProductCard,
   },
   setup(){
