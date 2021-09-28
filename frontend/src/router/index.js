963ce7ee9e92ab61336ec1b21aefd6e8ef5f589b
이영주 <clleo97@naver.com>
2021-09-28 11:36:43 +0900

    [FE] refactor: :hammer: Main.vue 관련 컨텐츠 변경

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Main from "@/views/Main.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import Main from "@/components/MainContent.vue";
 
 const routes = [
-    {
-        path: "/",
-        name: "Main",
-        component: Main,
-    },
-    {
-        path: '/search',
-        name: 'Search',
-        component: () => import("../views/Search.vue")
-      },
+  {
+    path: "/",
+    name: "Main",
+    component: Main,
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: () => import("../views/Search.vue"),
+  },
 ];
 
 const router = createRouter({
-    history: createWebHistory(process.env.BASE_URL),
-    routes
-})
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
 
-export default router
+export default router;
