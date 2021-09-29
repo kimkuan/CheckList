f536c5609c6a47f18423ba6052ab4c84eb07dc43
yjyjk20 <yjyjk20@naver.com>
2021-09-30 04:01:16 +0900

    [FE] design: :art: 제품 상세 페이지/리뷰 모달 추가 #8

---
+++ frontend/src/components/product/review/ProductDetailReviewModal.vue
@@ -0,0 +1,66 @@
+<template>
+  <div
+    class="modal"
+    tabindex="-1"
+    id="productDetailReviewModal"
+    ref="productDetailReviewModal"
+  >
+    <div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <div class="modal-title"><strong>'{{ word }}'에 대한 리뷰</strong></div>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+            @click="goParent"
+          ></button>
+          <hr>
+        </div>
+        <div class="row no-gutters">
+          <div class="modal-body p-5 d-flex align-items-center">
+            <div id="keyword-review">
+              <div v-for="item in 5" :key="item.id">
+                <product-detail-reivew-card></product-detail-reivew-card>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductDetailReivewCard from "../review/ProductDetailReviewCard.vue";
+
+export default {
+  name: "ProductDetailReviewModal",
+  props: {
+    word: String,
+  },
+  components: {
+    ProductDetailReivewCard,
+  },
+  data() {
+    return {
+      ProductDetailReviewModal: {},
+      activeDisplay: "flex",
+    };
+  },
+  setup(props, { emit }) {
+    const goParent = () => {
+      emit("call-parent");
+    };
+
+    return { goParent };
+  },
+};
+</script>
+
+<style scoped>
+  .modal {
+    display: block;
+  }
+</style>
