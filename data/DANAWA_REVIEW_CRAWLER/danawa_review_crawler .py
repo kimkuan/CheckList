109e60b48c468d1a39bb9742ac75535d59d7e77c
이영주 <clleo97@naver.com>
2021-09-30 23:33:45 +0900

    [DA] feat: :sparkles: 기존 파일 내용 삭제 및 try-catch 적용

--- data/DANAWA_REVIEW_CRAWLER/danawa_review_crawler .py
+++ data/DANAWA_REVIEW_CRAWLER/danawa_review_crawler .py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.common.exceptions import NoSuchElementException
 from scrapy.selector import Selector
 
 import datetime
@@ -84,7 +84,12 @@ class DanawaCrawler:
         crawlingName = categoryValue[STR_NAME]
         crawlingURL = categoryValue[STR_URL]
         crawlingSize = categoryValue[STR_CRAWLING_PAGE_SIZE]
-
+        
+        # 기존 csv 파일 데이터 삭제
+        if os.path.isfile(f'{crawlingName}.csv') :
+            os.remove(f'{crawlingName}.csv')
+            print(crawlingName+".csv 삭제!")
+            
         # data
         crawlingFile = open(f'{crawlingName}.csv', 'a', newline='', encoding='utf8')
         crawlingData_csvWriter = csv.writer(crawlingFile)
@@ -108,20 +113,57 @@ class DanawaCrawler:
         wait.until(EC.invisibility_of_element((By.CLASS_NAME, 'product_list_cover')))
         
         # 커피머신 전체 목록 가져오는 작업
-        for i in range(1, crawlingSize):
-            if i % 10 == 0:
-                browser.find_element_by_xpath('//a[@class="edge_nav nav_next"]').click()
+        for i in range(1, crawlingSize+1):
+            if i == 1:
+                print(1)
+            elif i % 10 == 1:
+                while True :
+                    try:
+                        browser.find_element_by_xpath('//a[@class="edge_nav nav_next"]').click()
+                        break
+                    except:
+                        print(os.error)
+                        print(crawlingName + " >> 전체 목록 페이지 중 ", i, " 선택 시 NoSuchElementException")
+                        browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=self.chrome_option)
+                        browser.implicitly_wait(15) # 웹페이지 로딩 기다리는 것 -> 5초로 늘려도 괜찮을 듯
+                        WebDriverWait(browser, 15)
+                        crawlingURL = categoryValue[STR_URL]
+                        browser.get(crawlingURL)
+                        print(crawlingURL)
+                        browser.find_element_by_xpath('//option[@value="90"]').click()
+                        wait = WebDriverWait(browser,5)
+                        # 'product_list_cover' : 로딩되기 전 페이지 -> 이게 안 보일 때까지 기다린다
+                        wait.until(EC.invisibility_of_element((By.CLASS_NAME, 'product_list_cover')))
+                        print("--------------------------------------------------------")
             elif i % 10 != 1:
-                browser.find_element_by_xpath('//a[@class="num "][%d]'%(i)).click()
-            elif i == 1:
-                browser.find_element_by_xpath('//li[@data-sort-method="NEW"]').click()
+                while True :
+                    try:
+                        browser.find_element_by_xpath('//a[@class="num "][%d]' % (i % 10)).click()
+                        break
+                    except:
+                        print(crawlingName + " >> 전체 목록 페이지 중 ", i, " 선택 시 NoSuchElementException")
+                        browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=self.chrome_option)
+                        browser.implicitly_wait(15) # 웹페이지 로딩 기다리는 것 -> 5초로 늘려도 괜찮을 듯
+                        WebDriverWait(browser, 15)
+                        crawlingURL = categoryValue[STR_URL]
+                        browser.get(crawlingURL)
+                        print(crawlingURL)
+                        browser.find_element_by_xpath('//option[@value="90"]').click()
+                        wait = WebDriverWait(browser,5)
+                        # 'product_list_cover' : 로딩되기 전 페이지 -> 이게 안 보일 때까지 기다린다
+                        wait.until(EC.invisibility_of_element((By.CLASS_NAME, 'product_list_cover')))
+                        print("--------------------------------------------------------")
+
             wait.until(EC.invisibility_of_element((By.CLASS_NAME, 'product_list_cover')))
             
             # html = browser.find_element_by_xpath('//div[@class="main_prodlist main_prodlist_list"]').get_attribute('outerHTML')
             # selector = Selector(text=html)
             
             # 목록에 있는 아이디 뽑아내기 & 리뷰 없는 건 그냥 건너뜀
-            productList = browser.find_elements_by_xpath('//li[@class="prod_item prod_layer "]')
+            if crawlingName == "Monitor":
+                productList = browser.find_elements_by_xpath('//li[@class="prod_item prod_layer width_change"]')
+            else:
+                productList = browser.find_elements_by_xpath('//li[@class="prod_item prod_layer "]')
 
             productIds = []
             productNames = []
@@ -143,18 +185,14 @@ class DanawaCrawler:
                     productNames.append(selector.xpath('//a[@name="productName"]/text()').get())
 
 
-            print("최종 >>", count)
-            # productIds = product.xpath('//@id').getall()
-            # if not productIds:
-            #     productIds = product.xpath('//li[@class="prod_item prod_layer width_change"]/@id').getall()
-            # productNames = product.xpath('//a[@name="productName"]/text()').getall()
+            print(crawlingName, " 현재 페이지 >> ", i, " / 최종 >>", count)
 
-            # 상품별 로직
+            #상품별 로직
             for j in range(len(productIds)) :
                 productId = productIds[j][11:]
                 productIdURL = f'&pcode={productId}'
                 productName = productNames[j].strip()
-                
+
                 # 이미 리뷰가 저장된 상품이면 다음 상품 진행
                 exit = False
                 for pcode in storedPcode :
@@ -165,6 +203,10 @@ class DanawaCrawler:
                 if exit == True :
                     print(productId + "는 이미 저장되었습니다 -> PASS")
                     continue
+
+                # 현재 저장하는 상품의 ID를 storedPcode.csv에 저장
+                storedPcode_cvsWriter.writerow([productId])
+                storedPcodeFile.flush()
 
                 browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=self.chrome_option)
                 browser.implicitly_wait(5) # 웹페이지 로딩 기다리는 것 -> 5초로 늘려도 괜찮을 듯
@@ -177,30 +219,68 @@ class DanawaCrawler:
                 
                 print(crawlingURL)
                 
-                if crawlingName == "Monitor" :
-                    browser.find_element_by_xpath('//a[@id="danawa-prodBlog-productOpinion-button-tab-companyReview"]').click()
-                
-                time.sleep(5)
-
                 # 상품
-                htmlProduct = browser.find_element_by_xpath('//*[@id="blog_content"]/div[2]/div[1]/div/div[1]/div/dl/dd/div/div/span[1]').get_attribute('outerHTML')
-                selectorProduct = Selector(text=htmlProduct)
-                productCategory = selectorProduct.xpath('//span/u/text()').get()
+                if crawlingName == "AirFryer" : 
+                    htmlProduct = browser.find_element_by_xpath('//*[@id="blog_content"]/div[2]/div[1]/div/div[1]/div/dl/dd/div/div/span[1]').get_attribute('outerHTML')
+                    selectorProduct = Selector(text=htmlProduct)
+                    productCategory = selectorProduct.xpath('//span/u/text()').get()
 
-                print(productCategory)
+                    if productCategory == '액세서리' :
+                        continue
 
-                if productCategory == '액세서리' :
-                    continue
+                if crawlingName == "Monitor" :
+                    monitorReview = browser.find_element_by_xpath('//div[@id="danawa-prodBlog-productOpinion-list-self"]').get_attribute('outerHTML')
+    
+                    selectorReview = Selector(text = monitorReview)
 
-                # 현재 저장하는 상품의 ID를 storedPcode.csv에 저장
-                storedPcode_cvsWriter.writerow([productId])
-                storedPcodeFile.flush() # 실시간 업데이트
+                    productReview1 = selectorReview.xpath('//a[@id="danawa-prodBlog-productOpinion-button-tab-companyReview"]').get()
+                    productReview2 = selectorReview.xpath('//a[@id="danawa-prodBlog-companyReview-button-tab-companyReview"]').get()
+
+                    print(productReview1)
+                    print(productReview2)
+                    while True :
+                        try:
+                            if productReview1 != None :
+                                browser.find_element_by_xpath('//a[@id="danawa-prodBlog-productOpinion-button-tab-companyReview"]').click()
+                            else :
+                                browser.find_element_by_xpath('//a[@id="danawa-prodBlog-companyReview-button-tab-companyReview"').click()
+                            break
+                        except:
+                            print("모니터 >> 리뷰 있나 없나 확인 시 NoSuchElementException")
+                            browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=self.chrome_option)
+                            browser.implicitly_wait(15) # 웹페이지 로딩 기다리는 것 -> 5초로 늘려도 괜찮을 듯
+                            WebDriverWait(browser, 15)
+                            browser.get(crawlingURL)
+                    
                 
-                htmlReview = browser.find_element_by_xpath('//div[@class="mall_review"]').get_attribute('outerHTML')
+                while True: 
+                    try:
+                        htmlReview = browser.find_element_by_xpath('//div[@class="mall_review"]').get_attribute('outerHTML')
+                        break
+                    except:
+                        print(crawlingName + "상품 리뷰 개수 세는 곳 : NoSuchElementException 발생")
+                        browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=self.chrome_option)
+                        browser.implicitly_wait(15) # 웹페이지 로딩 기다리는 것 -> 5초로 늘려도 괜찮을 듯
+                        WebDriverWait(browser, 15)
+                        browser.get(crawlingURL)
+                        if crawlingName == "Monitor" :
+                            monitorReview = browser.find_element_by_xpath('//div[@id="danawa-prodBlog-productOpinion-list-self"]').get_attribute('outerHTML')
+            
+                            selectorReview = Selector(text = monitorReview)
+
+                            productReview1 = selectorReview.xpath('//a[@id="danawa-prodBlog-productOpinion-button-tab-companyReview"]').get()
+                            productReview2 = selectorReview.xpath('//a[@id="danawa-prodBlog-companyReview-button-tab-companyReview"]').get()
+
+                            print(productReview1)
+                            print(productReview2)
+                            if productReview1 != None :
+                                browser.find_element_by_xpath('//a[@id="danawa-prodBlog-productOpinion-button-tab-companyReview"]').click()
+                            else :
+                                browser.find_element_by_xpath('//a[@id="danawa-prodBlog-companyReview-button-tab-companyReview"').click()
+                        continue
+
                 selectorReview = Selector(text=htmlReview)
                 productCrawlingSize = selectorReview.xpath('//div[@class="point_num"]/span[@class="cen_w"]/strong/text()').getall()
-                # print('>>> productId : ' + productId)
-                # print('>>> productName : ' + productName)
                 for k in range(len(productCrawlingSize)) :
                     # print('>>> 해당 상품 리뷰 데이터 개수 : ' + productCrawlingSize[k])
                     # 1000 단위로 ','이 들어오므로 해당 문자 제거
@@ -208,8 +288,7 @@ class DanawaCrawler:
 
                 print(productId)
                 if productCrawlingSize :
-                    # 리뷰 페이지마다
-                    # 리뷰 페이지 개수만큼
+
                     reviewCrawlingSize = int(int(productCrawlingSize[0]) / 10)
                     if int(productCrawlingSize[0]) % 10 != 0:
                         reviewCrawlingSize += 1
@@ -221,20 +300,47 @@ class DanawaCrawler:
                     print("크롤링할 페이지 개수 >>> ", reviewCrawlingSize)
 
                     for k in range(1, reviewCrawlingSize+1) :
-                        # print("********** cur page >> {}".format(k), '***********')
-                        if k % 10 == 0:
-                            browser.find_element_by_xpath('//a[@class="nav_edge nav_edge_next nav_edge_on"]').click()
-                        elif k % 10 != 1:
-                            # 한 페이지 클릭
-                            browser.find_element_by_xpath(f'/html/body/div[2]/div[3]/div[2]/div[4]/div[4]/div/div[3]/div[2]/div[2]/div[2]/div[5]/div/div/a[{(k-1) % 10}]').click()
-
+                        print("********** cur page >> {}".format(k), '***********')
+                        while True : 
+                            try:
+                                if k == 1:
+                                    print(1)
+                                elif k % 10 == 1:
+                                    browser.find_element_by_xpath('//a[@class="nav_edge nav_edge_next nav_edge_on"]').click()
+                                elif k % 10 != 1:
+                                    # 한 페이지 클릭
+                                    browser.find_element_by_xpath(f'/html/body/div[2]/div[3]/div[2]/div[4]/div[4]/div/div[3]/div[2]/div[2]/div[2]/div[5]/div/div/a[{(k-1) % 10}]').click()
+                                break
+                            except:
+                                print(crawlingName + " >> 리뷰 목록 페이지 선택 시 NoSuchElementException")
+                                browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=self.chrome_option)
+                                browser.implicitly_wait(15) # 웹페이지 로딩 기다리는 것 -> 5초로 늘려도 괜찮을 듯
+                                WebDriverWait(browser, 15)
+                                browser.get(crawlingURL)
+                                
                         # 한 페이지 내에서 최대 10개씩 리뷰 있음
                         wait = WebDriverWait(browser,5)                        
 
-                        time.sleep(5) 
+                        while True: 
+                            try:
+                                htmlReview = browser.find_element_by_xpath('//div[@class="mall_review"]').get_attribute('outerHTML')
+                                break
+                            except:
+                                print("리뷰 불러오는 곳 : NoSuchElementException 발생")
+                                browser = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=self.chrome_option)
+                                browser.implicitly_wait(15) # 웹페이지 로딩 기다리는 것 -> 5초로 늘려도 괜찮을 듯
+                                WebDriverWait(browser, 15)
+                                browser.get(crawlingURL)
+                                print("********** cur page >> {}".format(k), '***********')
+                                if k == 1:
+                                    print(1)
+                                elif k % 10 == 1:
+                                    browser.find_element_by_xpath('//a[@class="nav_edge nav_edge_next nav_edge_on"]').click()
+                                elif k % 10 != 1:
+                                    # 한 페이지 클릭
+                                    browser.find_element_by_xpath(f'/html/body/div[2]/div[3]/div[2]/div[4]/div[4]/div/div[3]/div[2]/div[2]/div[2]/div[5]/div/div/a[{(k-1) % 10}]').click()
 
                         # 이동한 페이지의 리뷰를 새로 읽어옴
-                        htmlReview = browser.find_element_by_xpath('//div[@class="mall_review"]').get_attribute('outerHTML')
                         selectorReview = Selector(text=htmlReview)
                         reviewList = selectorReview.xpath('//ul[@class="rvw_list"]')
                         reviewTitleList = reviewList.xpath('//div[@class="tit_W"]/p[@class="tit"]/text()').getall()
@@ -257,9 +363,6 @@ class DanawaCrawler:
                             reviewWriter = reviewWriterList[l]
                             reviewMall = reviewMallList[l]
                             print(reviewTitle)
-                            #print(reviewScore[0])
-                            # for m in range(len(reviewImgList)) : 
-                            #     print(reviewImgList[m])
                             print("---------------------------------------")
                             crawlingData_csvWriter.writerow([productId, crawlingName, productName, reviewTitle, reviewContent, reviewScore[0], reviewMall, reviewTime, reviewWriter, reviewImgList])
                 # 리뷰 제목 : <div class="tit_W"> <p class="tit">
