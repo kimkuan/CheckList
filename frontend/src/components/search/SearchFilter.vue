f64fe28a51fc9b5bd5d5ffe6d43c24dbd293fdba
sunhye <wkadnsj@gmail.com>
2021-09-30 21:13:58 +0900

    [FE] feat: :sparkles: add search tab

--- frontend/src/components/search/SearchFilter.vue
+++ frontend/src/components/search/SearchFilter.vue
@@ -1,16 +1,36 @@
 <template>
     <div>
-        <tabs>
-            <tab name="First tab">
-                First tab content
-            </tab>
-            <tab name="Second tab">
-                Second tab content
-            </tab>
-            <tab name="Third tab">
-                Third tab content
-            </tab>
+        <tabs v-model="selectedTab">
+            <tab
+            class="tab"
+            val="checkPick"
+            label="체크픽"
+            indicator="true"
+            />
+            <tab
+            class="tab"
+            val="filter"
+            label="직접탐색"
+            indicator="true"
+            />
         </tabs>
+
+        <tab-panels
+            v-model="selectedTab"
+            :animate="true"
+        >
+            <tab-panel
+            val="checkPick"
+            >
+            체크리스트에서 분석한 내용을 바탕으로 상품을 추천받아 보세요
+
+            </tab-panel>
+            <tab-panel
+            val="filter"
+            >
+            스펙을 원하는 대로 선택해서 상품을 찾을 수 있어요
+            </tab-panel>
+        </tab-panels>
     </div>
 </template>
 
