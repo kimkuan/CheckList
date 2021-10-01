c63ef268b8caf16cefe57dd27c20130752e51d8f
sunhye <wkadnsj@gmail.com>
2021-10-01 10:21:33 +0900

    [FE] feat: :sparkles: add SearchCategory

---
+++ frontend/src/components/search/SearchCategory.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="dropdown">
+        <select v-model="selected">
+            <option disabled value="category">카테고리</option>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+    </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name : "Category",
+    props : ['categories'],
+    components: {
+    },
+    setup(){
+    },
+    methods : {
+    }
+}
+</script>
+<style scoped>
+.dropdown {
+    text-align: center;
+}
+</style>
