743af23a5dbbd87b81d43bd8bf3e2cfbaf02e811
이영주 <clleo97@naver.com>
2021-10-02 23:13:42 +0900

    [FE] design: :art: 검색바 작업 & 최근 검색어 리스트 css #12

--- frontend/src/store/getters.js
+++ frontend/src/store/getters.js
@@ -4,3 +4,13 @@
 // export function getLoginUserInfo(state) {
 //   return state.loginUserInfo;
 // }
+
+// 상단 검색바에 친 검색어 가져오기
+export function getSearchWord(state) {
+  return state.searchWord;
+}
+
+// 검색어 히스토리 가져오기
+export function getSearchHistory(state) {
+  return state.searchHistory;
+}
