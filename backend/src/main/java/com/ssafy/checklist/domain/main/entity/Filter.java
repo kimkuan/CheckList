e0354e0cf0df7e9aca946d9c078252f52d0493c5
yjyjk20 <yjyjk20@naver.com>
2021-10-06 19:36:43 +0900

    [BE] feat: :sparkles: Entity 어노테이션 제거, id 어노테이션 제거 #40

--- backend/src/main/java/com/ssafy/checklist/domain/main/entity/Filter.java
+++ backend/src/main/java/com/ssafy/checklist/domain/main/entity/Filter.java
@@ -3,17 +3,13 @@ package com.ssafy.checklist.domain.main.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
-@Entity
 @Getter
 @Setter
 @Table(name = "filter")
 public class Filter {
 
-    @Id
     String category;
 
     String condition;
