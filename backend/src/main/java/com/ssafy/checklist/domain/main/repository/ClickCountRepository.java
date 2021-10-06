bc38cbd3ec10a0d5b1989f3fb6b20d8ecdd9efa3
yjyjk20 <yjyjk20@naver.com>
2021-10-06 17:40:40 +0900

    [BE] feat: :sparkles: 상품 조회수 엔티티, 레파지토리 추가

---
+++ backend/src/main/java/com/ssafy/checklist/domain/main/repository/ClickCountRepository.java
@@ -0,0 +1,11 @@
+package com.ssafy.checklist.domain.main.repository;
+
+import com.ssafy.checklist.domain.main.entity.ClickCount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClickCountRepository extends JpaRepository<ClickCount, Long> {
+
+    public <Click
+}
