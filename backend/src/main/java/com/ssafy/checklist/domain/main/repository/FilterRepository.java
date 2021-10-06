e6427530d597a7e108a138e7303d9981df7ab9a7
yjyjk20 <yjyjk20@naver.com>
2021-10-06 19:38:40 +0900

    [BE] feat: :sparkles: category별 필터구분 가져오는 쿼리메소드 추가 #40

--- backend/src/main/java/com/ssafy/checklist/domain/main/repository/FilterRepository.java
+++ backend/src/main/java/com/ssafy/checklist/domain/main/repository/FilterRepository.java
@@ -4,6 +4,10 @@ import com.ssafy.checklist.domain.main.entity.Filter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface FilterRepository extends JpaRepository<Filter, String> {
+
+    Optional<Filter> findByCategory(String category);
 }
