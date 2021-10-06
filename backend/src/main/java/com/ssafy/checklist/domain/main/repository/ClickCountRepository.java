696988c3b4b16680dc8901d614566f187fc6726c
yjyjk20 <yjyjk20@naver.com>
2021-10-06 19:39:41 +0900

    [BE] feat: :sparkles: 카운트 값 가져오는 쿼리메소드 추가, 카운트정보 페이징해서 가져오는 쿼리메소드 추가 #40

--- backend/src/main/java/com/ssafy/checklist/domain/main/repository/ClickCountRepository.java
+++ backend/src/main/java/com/ssafy/checklist/domain/main/repository/ClickCountRepository.java
@@ -1,11 +1,16 @@
 package com.ssafy.checklist.domain.main.repository;
 
 import com.ssafy.checklist.domain.main.entity.ClickCount;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ClickCountRepository extends JpaRepository<ClickCount, Long> {
 
-    public <Click
+    Optional<ClickCount> findByPcode(long pcode);
+    Page<ClickCount> findAll(Pageable page);
 }
