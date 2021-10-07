3aacf14bf356f4c01d5d291d30f82659c4a8c033
zxd9857@gmail.com <zxd9857@gmail.com>
2021-10-07 17:25:56 +0900

    [FE] bug: :bug: 미변경 포트번호 수정

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
 
     proxy: {
       '/api': {
-        target: 'http://localhost:80/'
+        target: 'http://localhost:8082/'
       },
     },
     historyApiFallback: true,
